44a7d607f8ece61055384127f270dbd565209206
jdhl27 <independientemedellin.27.03@gmail.com>
2023-09-18 15:07:19 -0500

    Add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Hotel King the land API')
+    .setDescription('Manejo de habitaciones y reservas del hotel')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3030);
 }
 bootstrap();
