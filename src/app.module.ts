a09db3e08892894399f9d0489884efde24f6aeb5
jdhl27 <independientemedellin.27.03@gmail.com>
2023-09-18 15:07:42 -0500

    Add mongoDB configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Room } from './client/domain/room.entity';
+import { Booking } from './client/domain/booking.entity';
+import { TypeRoom } from './client/domain/type-room.entity';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'hotel',
+      entities: [TypeRoom, Room, Booking],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
